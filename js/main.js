2cd7d8ccc505b63b52fb010f645e0f8253fa8317
ChipMorris10 <goldcomac@mac.com>
2015-09-11 15:16:06 -0600

    cleaned up the code to make it more readable, and added comments

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// var app = angular.module("myApp", [ ]);
+
+// app.controller("MyFirstController", function($scope){
+//   $scope.names = ["Harry", "Ron", "Hermione", "Sirius", "Hedwig", "Tonks"];
+// });
+
+var app = angular.module("firstApp", [ ]);
+
+app.controller("MyFirstController", function($scope){
+    $scope.name = "Severus Snape";
+});
+
+
